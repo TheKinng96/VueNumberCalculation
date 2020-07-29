9bc476dd36e29ee68928cad40ed6a819c24669e8
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-07-29 16:12:18 +0900

    added mobile support

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 title">
         <h1 class="text-center">The Super Quiz</h1>
       </div>
     </div>
